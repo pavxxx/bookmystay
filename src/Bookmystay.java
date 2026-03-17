8c487bfc9b85d87352ff94377b37ec04ec3dcd99
pavxxx <pavithra020906@gmail.com>
2026-03-17 12:22:09 +0530

    uc2

--- src/Bookmystay.java
+++ src/Bookmystay.java
@@ -1,30 +1,102 @@
 /**
- * UseCase1HotelBookingApp
+ * UseCase2RoomInitialization
  *
- * This class represents the entry point of the Book My Stay
- * Hotel Booking Management System application.
- * It demonstrates how a Java application starts execution
- * and prints a welcome message to the console.
+ * Demonstrates basic room types using abstraction,
+ * inheritance and static availability.
  *
  * @author Student
- * @version 1.0
+ * @version 2.1
  */
 
 public class Bookmystay {
 
-    /**
-     * The main method is the starting point of the application.
-     * JVM invokes this method when the program is executed.
-     *
-     * @param args Command line arguments
-     */
     public static void main(String[] args) {
 
-        // Display welcome message
         System.out.println("=================================");
-        System.out.println(" Welcome to Book My Stay ");
-        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("Book My Stay - Hotel Booking App");
+        System.out.println("Version 2.1");
         System.out.println("=================================");
 
+        // Creating room objects
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        // Static availability variables
+        int singleAvailability = 10;
+        int doubleAvailability = 5;
+        int suiteAvailability = 2;
+
+        System.out.println("\nRoom Details:\n");
+
+        singleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleAvailability);
+        System.out.println();
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleAvailability);
+        System.out.println();
+
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteAvailability);
+    }
+}
+
+
+/**
+ * Abstract class representing a Room
+ */
+abstract class Room {
+
+    protected String type;
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(String type, int beds, int size, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Room Size: " + size + " sq.ft");
+        System.out.println("Price per Night: $" + price);
+    }
+}
+
+
+/**
+ * Single Room class
+ */
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 200, 100);
+    }
+}
+
+
+/**
+ * Double Room class
+ */
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 300, 180);
+    }
+}
+
+
+/**
+ * Suite Room class
+ */
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 500, 300);
     }
 }
